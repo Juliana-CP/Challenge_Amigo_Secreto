de2bfe46e5917089c2d091e4c09206a90bd41ef8
Juliana Pereira <juliejuly001@gmail.com>
2025-02-16 18:14:39 -0300

    Correçao do nome da lista

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
 
-let listadenomes = [] ;
+let listaDeNomes = [] ;
 function adicionarAmigo() {
     let nome = document.querySelector("input").value ;
     console.log(nome) ;
 
     if(nome.trim() == "") {
-        alert("por favor, insira um nome.")
+        alert("Por favor, insira um nome.")
     } else {
-        listadenomes.push(nome) ;
+        listaDeNomes.push(nome) ;
         document.querySelector("input").value = "" ;
-        console.log(listadenomes) ;
+        console.log(listaDeNomes) ;
 
     }
     
