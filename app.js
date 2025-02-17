b1f5f64a1686db3a82c0a3c599ce0cc946f1ca52
Juliana Pereira <juliejuly001@gmail.com>
2025-02-16 21:17:29 -0300

    Mostrando o amigo sorteado

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
 
-let listaDeNomes = [] ;
+let amigos = [] ;
 function adicionarAmigo() {
     let nome = document.querySelector("input").value ;
     console.log(nome) ;
@@ -8,12 +8,12 @@ function adicionarAmigo() {
     if(nome.trim() == "") {
         alert("Por favor, insira um nome.")
     } else {
-        listaDeNomes.push(nome) ;
+        amigos.push(nome) ;
         document.querySelector("input").value = "" ;
-        console.log(listaDeNomes) ;
+        console.log(amigos) ;
 
         atualizarListaDeAmigos() ;
-
+        
     }
     
 }
@@ -22,10 +22,24 @@ function atualizarListaDeAmigos() {
     const lista = document.getElementById("listaAmigos") ;
     lista.innerHTML = "" ;
 
-    listaDeNomes.forEach(nome => {
+    amigos.forEach(nome => {
         let li = document.createElement("li") ;
         li.textContent = nome ;
         lista.appendChild(li) ;
     }) ;
 
+}
+
+function sortearAmigo() {
+    if(amigos.length === 0) {
+        alert( "A lista está vazia. Adicione nomes antes de sortear.") ;
+        return;
+    }
+
+    const indiceAleatorio = Math.floor(Math.random() * amigos.length) ;
+    const nomeSorteado = amigos[indiceAleatorio];
+
+    document.getElementById("resultado").textContent = `O amigo sorteado é: ${nomeSorteado}`;
+
+    
 }
